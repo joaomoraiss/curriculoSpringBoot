e040ed4edfdc94d7decba1a1ba164f6aa99dd330
joaomoraiss <joaovictorrmpework@outlook.com>
2024-06-03 17:54:35 -0300

    Initial commit

---
+++ src/test/java/com/example/curriculo/CurriculoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
